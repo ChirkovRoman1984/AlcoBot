48eaf9ef20862d02c34973feeaa3b4367b21edbf
ChirkovRoman1984 <chirkovroman1984@gmail.com>
2022-12-10 15:14:33 +0400

    poll

--- handlers/quiz/main.py
+++ handlers/quiz/main.py
@@ -1,9 +1,9 @@
 from aiogram import Dispatcher
-from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters import Text, IDFilter
 
-from handlers.quiz.quiz import cmd_quiz, quiz_callback
+from handlers.quiz.quiz import quiz_callback, input_quiz
 
 
 def register_quiz_handlers(dp: Dispatcher) -> None:
     dp.register_callback_query_handler(quiz_callback, Text(startswith=['quiz@']))
-    dp.register_message_handler(cmd_quiz, chat_type='private', commands=["quiz"])
+    dp.register_message_handler(input_quiz, IDFilter(user_id=176814724), content_types='poll', chat_type='private')
